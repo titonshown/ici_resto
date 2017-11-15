8761147d4710bf282d6d908c85d9abc15fe62c2c
Claudia997 <30850291+Claudia997@users.noreply.github.com>
2017-11-14 23:44:09 -0300

    Cambio básico de aspecto
    -Cambio de colores
    -Cambio de proporciones
    -Mención de miembros del equipo

--- gUI/Ventana.java
+++ gUI/Ventana.java
@@ -1,4 +1,4 @@
-package gUI;
+-package gUI;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
@@ -15,17 +15,19 @@ import javax.swing.border.EmptyBorder;
 
 import iciresto.Administrador;
 
-public class Ventana extends JFrame implements ActionListener {
 
+public class Ventana extends JFrame implements ActionListener {
+	
 	private JPanel contentPane;
 	private JButton btnLogin;
 	
+	
 	public Ventana() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1024, 768);
+		setBounds(100,0, 1024, 720);
 		contentPane = new JPanel();
-		contentPane.setBackground(Color.BLUE);
-		contentPane.setForeground(Color.RED);
+		contentPane.setBackground(new Color(128, 0, 0));
+		contentPane.setForeground(new Color(0, 0, 0));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(null);
 		setContentPane(contentPane);
@@ -36,17 +38,25 @@ public class Ventana extends JFrame implements ActionListener {
 		JLabel lblIciresto = new JLabel("ICI-Resto");
 		lblIciresto.setBounds(302, 310, 413, 113);
 		lblIciresto.setFont(new Font("Arial Black", Font.PLAIN, 80));
-		lblIciresto.setForeground(Color.RED);
+		lblIciresto.setForeground(new Color(218, 165, 32));
 		contentPane.add(lblIciresto);
+		
+		JLabel creators = new JLabel("C.Garay, B.Lebrecht, H.Sepulveda (2017)");
+		creators.setBounds(370,650,500,100);
+		creators.setFont(new Font("Arial Black", Font.PLAIN, 12));
+		creators.setForeground(new Color(218, 165, 32));
+		contentPane.add(creators);
+		
 		
 		btnLogin = new JButton("Login");
 		btnLogin.setBounds(770, 632, 97, 25);
-		btnLogin.setForeground(Color.BLACK);
-		btnLogin.setBackground(Color.RED);
+		btnLogin.setForeground(new Color(120,20,40));
+		btnLogin.setBackground(new Color(230, 200, 130));
 		btnLogin.setFont(new Font("Arial Black", Font.PLAIN, 13));
 		btnLogin.addActionListener(this);
 		contentPane.add(btnLogin);
 		}
+		
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
